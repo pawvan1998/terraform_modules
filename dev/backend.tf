87d237b557d64c8c20d4df4fd4e56233ecbe2c6a
pawvan1998 <55293135+pawvan1998@users.noreply.github.com>
2024-01-08 19:40:59 +0530

    Update backend.tf

--- dev/backend.tf
+++ dev/backend.tf
@@ -1,9 +1,16 @@
-terraform {
-  cloud {
-    organization = "aws-cloud-tf"
+#terraform {
+ # cloud {
+  #  organization = "aws-cloud-tf"
 
-    workspaces {
-      name = "dev-aws-terraform-pawan-nw"
-    }
+   # workspaces {
+    #  name = "dev-aws-terraform-pawan-nw"
+    #}
+  #}
+
+terraform {
+  backend "s3" {
+    bucket = "aws-hello.com"
+    key    = "terraformstates/dev.tfstate"
+    region = "ap-south-1"
   }
 }
