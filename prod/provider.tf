8b91818e16d4a7902509a5a7271371a3412eddbc
pawvan1998 <55293135+pawvan1998@users.noreply.github.com>
2024-01-08 19:43:22 +0530

    Update provider.tf

--- prod/provider.tf
+++ prod/provider.tf
@@ -1,11 +1,19 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
-}
+#terraform {
+ # required_providers {
+  #  aws = {
+   #   source = "hashicorp/aws"
+    #}
+  #}
+#}
 
 provider "aws" {
   region = "ap-south-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "aws-hello.com"
+    key    = "terraformstates/dev.tfstate"
+    region = "ap-south-1"
+  }
 }
